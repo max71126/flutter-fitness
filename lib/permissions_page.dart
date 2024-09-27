63b128db1f7638f96318acc11b81167c5aee1a8b
Max <max.bright429@gmail.com>
2024-09-27 14:24:51 -0400

    Update asset
    Co-authored-by: keen71126 <keen.mr429@gmail.com>

--- lib/permissions_page.dart
+++ lib/permissions_page.dart
@@ -196,3 +196,5 @@ class _PermissionsPageState extends State<PermissionsPage> {
 // Auto-generated comment for change 15
 
 // Auto-generated comment for change 15
+
+// Auto-generated comment for change 15
