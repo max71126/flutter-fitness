7b058c6b1d43aeda4dae8724f2404b82d0b15bd6
Max <max.bright429@gmail.com>
2024-10-02 02:17:45 -0400

    Update asset
    Co-authored-by: keen71126 <keen.mr429@gmail.com>

--- lib/graphs_filters.dart
+++ lib/graphs_filters.dart
@@ -89,3 +89,5 @@ class _GraphsFiltersState extends State<GraphsFilters> {
 // Auto-generated comment for change 11
 
 // Auto-generated comment for change 11
+
+// Auto-generated comment for change 11
